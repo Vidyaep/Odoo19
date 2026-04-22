6edc4e7bb2ea0ea433a88bce005be278c80cfad2
cybrosys <vidyaraghunathan22@gmail.com>
2026-04-22 17:15:55 +0530

    [UPDT]Hotel Management Report(2)

--- hotel_management/models/hotel_report.py
+++ hotel_management/models/hotel_report.py
@@ -1,19 +1,22 @@
 from odoo import api, fields, models
+from odoo.exceptions import UserError
+
+
 class HotelReport(models.TransientModel):
     _name = 'hotel.report'
     date_from = fields.Date(required=True,string='Date from')
     date_to = fields.Date(required=True,string='Date to')
     guest_name = fields.Many2one('res.partner',string='Guest Name')
+    company_id = fields.Many2one('res.company',string='Company',default=lambda self: self.env.company)
 
     def generate(self):
-        print(self.read()[0])
-        query = """select sequence,guests,check_in,check_out,status from hotel_accommodation where check_in >= '%s' and check_out <= '%s' and guests = '%s'""" % (self.date_from, self.date_to, self.guest_name.id)
-        self.env.cr.execute(query)
-        report = self.env.cr.fetchall()
-        print(report)
-        data = {
-            'report': report,
-            'form' : self.read()[0]
-        }
-        return self.env.ref('hotel_management.action_hotel_management_report').report_action(self,data=data)
+        # print(self.read()[0])
+        query = """select a.sequence,a.guests,r.name,a.check_in,a.check_out,a.status from hotel_accommodation as a left join res_partner as r on a.guests=r.id where a.guests = %s"""
+        self.env.cr.execute(query,self.guest_name.id,)
+        results = self.env.cr.dictfetchall()
+        print(results)
+        if results:
+           return self.env.ref('hotel_management.action_hotel_management_report').report_action(results)
+        else:
+            raise UserError("No result found")
 

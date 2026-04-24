8f90c7a74b024eff93393d429b9176638ac14f5a
cybrosys <vidyaraghunathan22@gmail.com>
2026-04-24 17:23:10 +0530

    [UPDT]Hotel Management Report(3)

--- hotel_management/models/hotel_report.py
+++ hotel_management/models/hotel_report.py
@@ -1,9 +1,13 @@
+from time import strptime
+
 from odoo import api, fields, models
+from odoo.addons.test_convert.tests.test_env import data
 from odoo.exceptions import UserError
 
 
 class HotelReport(models.TransientModel):
     _name = 'hotel.report'
+    _description = 'Hotel Report Model'
     date_from = fields.Date(required=True,string='Date from')
     date_to = fields.Date(required=True,string='Date to')
     guest_name = fields.Many2one('res.partner',string='Guest Name')
@@ -11,12 +15,24 @@ class HotelReport(models.TransientModel):
 
     def generate(self):
         # print(self.read()[0])
-        query = """select a.sequence,a.guests,r.name,a.check_in,a.check_out,a.status from hotel_accommodation as a left join res_partner as r on a.guests=r.id where a.guests = %s"""
-        self.env.cr.execute(query,self.guest_name.id,)
+        query = """select a.sequence,r.name,a.check_in,a.check_out,a.status from hotel_accommodation as a left join res_partner as r on a.guests=r.id where a.active=True"""
+        if self.guest_name:
+            query += """ and a.guests = %s""" %(self.guest_name.id)
+            # self.env.cr.execute(query, (self.guest_name.id,))
+        # date_from = str(self.date_from)
+        # date_checkin = strptime(date_from, '%Y-%m-%d')
+        if self.date_from:
+            query += """ and a.check_in >= '%s' and a.check_out < '%s'""" %(self.date_from, self.date_to)
+        #     # self.env.cr.execute(query,(self.date_from,))
+        # date_to = str(self.date_to)
+        # date_checkout = strptime(date_to, '%Y-%m-%d')
+        # if self.date_to:
+        #     query += """ and a.check_out >= '%s'""" %(self.date_to)
+        #     # self.env.cr.execute(query,(self.date_to,))
+        self.env.cr.execute(query)
         results = self.env.cr.dictfetchall()
         print(results)
-        if results:
-           return self.env.ref('hotel_management.action_hotel_management_report').report_action(results)
-        else:
-            raise UserError("No result found")
+        return results
 
+    def cancel(self):
+        self.clear()
